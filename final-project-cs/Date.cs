fbdbbab960f3f1ec91185cfd8c013b99158870ff
IturielLSaenz <ilsaenzacademic@gmail.com>
2025-05-03 17:48:17 -0600

    Create Date.cs
    Class date, for handling dates and appointments

---
+++ final-project-cs/Date.cs
@@ -0,0 +1,10 @@
+/*
+    Class Date, donde se maneja la creación de objetos "Fecha"
+*/
+using System;
+public class Date{
+    private int d,m,y;
+    public Date(int d=1, int m=1,int y=1990){
+        
+    }
+}
