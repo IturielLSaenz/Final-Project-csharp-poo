402a0891644ac9b50022ac6231f67eceed581938
IturielLSaenz <ilsaenzacademic@gmail.com>
2025-05-03 18:32:24 -0600

    Update Date.cs
    Updated main functionality for date.cs

--- final-project-cs/Date.cs
+++ final-project-cs/Date.cs
@@ -4,7 +4,51 @@
 using System;
 public class Date{
     private int d,m,y;
-    public Date(int d=1, int m=1,int y=1990){
-        
+    public Date(int d=1, int m=1,int y=1970){
+        this.Day=d;
+        this.Month=m;
+        this.Year=y;
     }
+    /*
+        Hacemos primero el setter del año
+        así es más facil calcular si se trata de un año bisiesto
+        y poder asignar los valores del día con mejores condiciones!
+    */
+    public int Year{
+        set{
+            this.y = value>=1970 ? value:1970;
+        }get{return this.y;}
+    }
+    public int Month{
+        set{
+            this.m = (value>0 && value<=12) ? value:1;
+        }get{return this.m;}
+    }
+    public int Day{
+        set{
+            // logica para ingresar el dia
+            int limit;
+            if (Array.Exists(new int[] { 1, 3, 5, 7, 8, 10, 12 }, month => month == this.Month)){
+                limit = 31;
+            }else if(Array.Exists(new int[] {4,6,9,11}, month => month == this.Month)){
+                limit = 30;
+            }else{
+                if (isLeapYear()){
+                    limit = 29;
+                }else{
+                    limit = 28;
+                }
+            }
+            // asignar el valor:
+            if(value<=limit){
+                this.d = value;
+            }else{
+                Console.WriteLine("No es un valor válido para el día en ese mes y año!");
+            }
+        }get{return this.d;}
+    }
+    public bool isLeapYear(){
+        return Year%4==0 && (Year%100!=0 || Year%400==0);
+    }
+
 }
