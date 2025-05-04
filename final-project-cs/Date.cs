a214fb32baabe247fdaa3965ea93eacb16ef1186
IturielLSaenz <ilsaenzacademic@gmail.com>
2025-05-04 15:14:37 -0600

    Update Date.cs
    Full implementation, reduced unnecesary code

--- final-project-cs/Date.cs
+++ final-project-cs/Date.cs
@@ -28,18 +28,7 @@ public class Date{
     public int Day{
         set{
             // logica para ingresar el dia
-            int limit;
-            if (Array.Exists(new int[] { 1, 3, 5, 7, 8, 10, 12 }, month => month == this.Month)){
-                limit = 31;
-            }else if(Array.Exists(new int[] {4,6,9,11}, month => month == this.Month)){
-                limit = 30;
-            }else{
-                if (isLeapYear()){
-                    limit = 29;
-                }else{
-                    limit = 28;
-                }
-            }
+            int limit = getLimitDay();
             // asignar el valor:
             if(value<=limit){
                 this.d = value;
@@ -48,20 +37,53 @@ public class Date{
             }
         }get{return this.d;}
     }
-    public bool isLeapYear(){
-        return Year%4==0 && (Year%100!=0 || Year%400==0);
-    }
     // metodos para incrementar y decerementar los atributos:
         public void inrementYear(){
         this.Year +=1;
     }
         public void incrementMonth(){
-        if(this.Month+1>12){
-            this.Month=1;
-            this.inrementYear();
+        if(Month+1>12){
+            Month=1;
+            inrementYear();
         }
     }
     public void incrementDay(){
+        int limit = getLimitDay();
+        if(Day+1>limit){
+            Day=1;
+            incrementMonth();
+        }else{
+            Day+=1;
+        }        
+    }
+    public void decrementYear(){
+        if(Year-1>=1970){
+            Year-=1;
+        }else{
+            Console.WriteLine("No puedes disminuir más el año!");
+        }
+    }
+    public void decrementMonth(){
+        if(Month-1>=1){
+            Month-=1;
+        }else{
+            Month=1;
+            decrementYear();
+        }
+    }
+    public void decrementDay(){
+        int limit = getLimitDay();
+        if(Day-1<=limit){
+            Day-=1;
+        }else{
+            Day=1;
+            decrementMonth();
+        }
+    }
+    public bool isLeapYear(){
+        return Year%4==0 && (Year%100!=0 || Year%400==0);
+    }
+    public int getLimitDay(){
         int limit;
         if (Array.Exists(new int[] { 1, 3, 5, 7, 8, 10, 12 }, month => month == this.Month)){
             limit = 31;
@@ -74,23 +96,12 @@ public class Date{
                 limit = 28;
             }
         }
-        if(this.Day+1>limit){
-            this.Day=1;
-            this.incrementMonth();
-        }else{
-            this.Day+=1;
-        }        
+        return limit;
     }
-    public void decrementDay(){
-
-    }
-    public void decrementMonth(){
-
-    }
-    public void decrementYear(){
-
-    }
+    /*
+        Metodo toString para el display del objeto:
+    */
     public string toString(){
-        return $"Fecha: {Day}/{Month}/{Year}";
+        return $"{Day}/{Month}/{Year}";
     }
 }
